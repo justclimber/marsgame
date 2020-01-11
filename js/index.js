23c9805255260f4e620c146c1923c5d46277d2a7
Aleksandr Akimov <climber.business@gmail.com>
2020-01-11 10:38:36 +0300

    replace vx and vy by throttle - moving forward or backward with some speed

--- js/index.js
+++ js/index.js
@@ -1,20 +1,22 @@
 import * as PIXI from 'pixi.js'
 
 const app = new PIXI.Application({
-    backgroundColor: "0xffffff"
+    backgroundColor: "0xffffff",
 });
 
 let mech;
 
 function initMechVars() {
-    mech.scale.set(0.3, 0.3);
+    mech.scale.set(0.2, 0.2);
+    mech.scale.y *= -1;
     mech.anchor.set(0.5);
-    mech.x = 100;
-    mech.y = 100;
+    mech.x = app.screen.width / 2 - mech.width / 2;
+    mech.y = app.screen.height / 2 - mech.height / 2;
     mech.vx = 0;
     mech.vy = 0;
     mech.vr = 0;
-    mech.rotation = Math.PI;
+    mech.throttle = 0;
+    mech.rotation = 0;
 }
 
 function parseResponse(result) {
@@ -22,11 +24,11 @@ function parseResponse(result) {
     let errorContainer = document.getElementById("errorsContainer");
     if (result.error) {
         let errorTextContainer = document.getElementById("errorsText");
-        errorTextContainer.innerHTML = result.error.replace(/\n/g, '<br/>')
+        errorTextContainer.innerHTML = result.error.replace(/\n/g, '<br/>');
 
-        errorContainer.style.display = 'block'
+        errorContainer.style.display = 'block';
     } else {
-        errorContainer.style.display = 'none'
+        errorContainer.style.display = 'none';
         updateMechVars(result)
     }
 }
@@ -38,21 +40,25 @@ function updateMechVars(result) {
             mech.vr = vr;
         }
     }
-    if (result.vx) {
-        let vx = parseFloat(result.vx);
-        if (vx === vx) {
-            mech.vx = vx;
+    if (result.rotation) {
+        let rotation = parseFloat(result.rotation);
+        if (rotation === rotation) {
+            mech.rotation = rotation;
         }
     }
-    if (result.vy) {
-        let vy = parseFloat(result.vy);
-        if (vy === vy) {
-            mech.vy = vy;
+    if (result.throttle) {
+        let throttle = parseFloat(result.throttle);
+        if (throttle === throttle) {
+            mech.throttle = throttle
         }
     }
 }
 
 function gameLoop(delta) {
+    if (mech.throttle) {
+        mech.vx = Math.cos(mech.rotation + Math.PI/2) * mech.throttle;
+        mech.vy = Math.sin(mech.rotation + Math.PI/2) * mech.throttle;
+    }
     mech.x += mech.vx;
     mech.y += mech.vy;
     mech.rotation += mech.vr;
@@ -62,6 +68,7 @@ function resetVelocity() {
     mech.vx = 0;
     mech.vy = 0;
     mech.vr = 0;
+    mech.throttle = 0;
 }
 
 window.onload = function() {
