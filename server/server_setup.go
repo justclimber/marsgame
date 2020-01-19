326403bbdc56d161012bf2a0467df0a71552cb6d
Aleksandr Akimov <climber.business@gmail.com>
2020-01-19 19:36:23 +0300

    remove old code

--- server/server_setup.go
+++ server/server_setup.go
@@ -1,7 +1,6 @@
 package server
 
 import (
-	"encoding/json"
 	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
@@ -26,35 +25,8 @@ func (s *Server) wsEndpoint(w http.ResponseWriter, r *http.Request) {
 	client.Listen()
 }
 
-type saveCode struct {
-	UserId     string `json:"userId"`
-	SourceCode string `json:"sourceCode"`
-}
-
-func (s *Server) saveSourceCodeEndpoint(w http.ResponseWriter, r *http.Request) {
-	var sc saveCode
-	err := json.NewDecoder(r.Body).Decode(&sc)
-	if err != nil {
-		log.Fatal("parsing error" + err.Error())
-	}
-
-	s.saveSourceCode(sc.UserId, sc.SourceCode)
-}
-
-func (s *Server) programFlowCmdEndpoint(w http.ResponseWriter, r *http.Request) {
-	var pf ProgramFlow
-	err := json.NewDecoder(r.Body).Decode(&pf)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	s.programFlowCmd(pf.UserId, pf.FlowCmd)
-}
-
 func (s *Server) Setup() {
 	go s.ListenClients()
 	http.Handle("/", http.FileServer(http.Dir("./frontend/static")))
 	http.HandleFunc("/ws", s.wsEndpoint)
-	http.HandleFunc("/save_source_code", s.saveSourceCodeEndpoint)
-	http.HandleFunc("/program_flow", s.programFlowCmdEndpoint)
 }
